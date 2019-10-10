4a2f78bdc9a6f5f8933e1f1cfee2b52884187b8f
wemlion <angusfu1126@qq.com>
2019-10-10 15:53:52 +0800

    feat: support auto publishing

--- .releaserc.js
+++ .releaserc.js
@@ -1,1 +1,10 @@
-module.exports = require("./index");
+const config = require("./index");
+
+module.exports = {
+  ...config,
+  plugins: [
+    ...config.plugins.slice(0, -1),
+    "@semantic-release/npm",
+    ...config.plugins.slice(-1)
+  ]
+};
